8765996d68025bf88c31944d9234bbc420f16ae1
pavansw <pavanwankhade4u@gmail.com>
2021-02-16 10:57:11 +0000

    Update first.py

--- first.py
+++ first.py
@@ -2,10 +2,10 @@ print ("This is a public Python code having many collaborators")
 x = 200
 y = 200
 z = 300
-print (x+y+z," is addition of ",x,y" and ",z)
+print (x+y+z," is addition of ",x,y," and ",z)
 print ("Adding new line here to demo Conflicts")
-print (x*y*z," is multiplication of ",x,y" and ",z)
-print(x-y-z,'is subtraction of ',x,y' and ',z)
+print (x*y*z," is multiplication of ",x,y," and ",z)
+print(x-y-z,'is subtraction of ',x,y,' and ',z)
 print(x/y/z,'is the division')
 print("added division")
 print(x+y-z," is an operation on ",x,y,z)
